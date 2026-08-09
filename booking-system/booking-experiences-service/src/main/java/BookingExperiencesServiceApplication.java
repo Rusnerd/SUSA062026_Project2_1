1438c8b32689818522958f4a24d6156184ea8226
Rusnerd <bratal@abv.bg>
2026-08-09 05:53:59 +0300

    feat first implementations

---
+++ booking-system/booking-experiences-service/src/main/java/BookingExperiencesServiceApplication.java
@@ -0,0 +1,16 @@
+package org.example.bookingexperiences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class
+})
+public class BookingExperiencesServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookingExperiencesServiceApplication.class, args);
+    }
+}
---
+++ booking-system/booking-main/src/main/java/BookingMainApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMainApplication {
+    public static void main(String[] args) {
+        System.out.println("Starting BookingMainApplication...");
+        SpringApplication.run(BookingMainApplication.class, args);
+    }
+}
